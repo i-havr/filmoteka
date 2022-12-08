3192e319498281c00bae164443c245de67f6b8c4
Oleksandr Perlov <oleksandrperlov@gmail.com>
2022-12-08 19:44:03 +0200

    add new photos

--- src/js/modal-students.js
+++ src/js/modal-students.js
@@ -1,19 +1,19 @@
-const refs = {
-  openLink: document.querySelector('.footer__link'),
-  modal: document.querySelector('#my-modal'),
-  backdrop: document.querySelector('#my-backdrop'),
-  close: document.querySelector('#my-close-button'),
-};
+// const refs = {
+//   openLink: document.querySelector('.footer__link'),
+//   modal: document.querySelector('#my-modal'),
+//   backdrop: document.querySelector('#my-backdrop'),
+//   close: document.querySelector('#my-close-button'),
+// };
 
-refs.openLink.addEventListener('click', onOpen);
-refs.close.addEventListener('click', onClose);
-refs.backdrop.addEventListener('click', onClose);
+// refs.openLink.addEventListener('click', onOpen);
+// refs.close.addEventListener('click', onClose);
+// refs.backdrop.addEventListener('click', onClose);
 
-function onOpen(e) {
-  e.preventDefault();
-  refs.backdrop.classList.toggle('hidden');
-}
+// function onOpen(e) {
+//   e.preventDefault();
+//   refs.backdrop.classList.toggle('hidden');
+// }
 
-function onClose() {
-  refs.backdrop.classList.toggle('hidden');
-}
+// function onClose() {
+//   refs.backdrop.classList.toggle('hidden');
+// }
