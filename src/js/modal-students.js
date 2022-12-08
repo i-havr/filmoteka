6048a9cdb633638e1cdb29d728db29a071f38a48
ksennis <105414714+ksennis@users.noreply.github.com>
2022-12-08 19:13:43 +0200

    import style (new way), style for modal

--- src/js/modal-students.js
+++ src/js/modal-students.js
@@ -5,14 +5,17 @@ const refs = {
   close: document.querySelector('#my-close-button'),
 };
 
-// refs.openLink.addEventListener('click', onOpen);
-// refs.close.addEventListener('click', onClose);
+refs.openLink.addEventListener('click', onOpen);
+refs.close.addEventListener('click', onClose);
+refs.backdrop.addEventListener('click', onClose);
 
-// function onOpen(e) {
-//   e.preventDefault();
-//   refs.backdrop.classList.toggle('hidden');
-// }
+function onOpen(e) {
+  e.preventDefault();
+  refs.backdrop.classList.toggle('hidden');
+}
 
-// function onClose() {
-//   refs.backdrop.classList.toggle('hidden');
-// }
+function onClose() {
+  refs.backdrop.classList.toggle('hidden');
+}
+
+
--- src/js/modal.js
+++ src/js/modal.js
@@ -1,77 +1,78 @@
-export default class Modal {
-    constructor({ container }) {
-        this.container = container;
+// export default class Modal {
+//     constructor({ container }) {
+//         this.container = container;
 
-        this.container.addEventListener('click', this.handleClickOpen.bind(this));
-    }
+//         this.container.addEventListener('click', this.handleClickOpen.bind(this));
+//     }
 
-    render() {
-        this.renderLayout();
+//     render() {
+//         this.renderLayout();
 
-        this.addEventListeners();
-    }
+//         this.addEventListeners();
+//     }
 
-    onOpenModal() {
+//     onOpenModal() {
 
-    }
+//     }
 
-    getRefs(modalEl) {
-        this.contentContainerEl = modalEl.querySelector(".modal__content");
-        this.closeEl = modalEl.querySelector(".modal__close-button");
-        this.overlayEl = modalEl.querySelector(".modal__overlay");
-    }
+//     getRefs(modalEl) {
+//         this.contentContainerEl = modalEl.querySelector(".modal__content");
+//         this.closeEl = modalEl.querySelector(".modal__close-button");
+//         this.overlayEl = modalEl.querySelector(".modal__overlay");
+//     }
 
-    renderLayout() {
-        this.modalEl = document.createElement("div");
-        this.modalEl.classList.add("modal");
+//     renderLayout() {
+//         this.modalEl = document.createElement("div");
+//         this.modalEl.classList.add("modal");
 
-        const layout = `
-            <div class="modal__overlay"></div>
-            <div class="modal__content-container">
-                <div class="modal__close-button">
-                    x
-                </div>
-                <div class="modal__content">
+//         const layout = `
+//             <div class="modal__overlay"></div>
+//             <div class="modal__content-container">
+//                 <div class="modal__close-button">
+//                     x
+//                 </div>
+//                 <div class="modal__content">
                 
-                </div>
-            </div> 
-        `;
+//                 </div>
+//             </div> 
+//         `;
 
-        this.modalEl.insertAdjacentHTML("beforeend", layout);
-        document.body.append(this.modalEl);
+//         this.modalEl.insertAdjacentHTML("beforeend", layout);
+//         document.body.append(this.modalEl);
 
-        const contenEl = this.renderContent();
+//         const contenEl = this.renderContent();
 
-        this.getRefs(this.modalEl);
-        this.contentContainerEl.append(contenEl);
+//         this.getRefs(this.modalEl);
+//         this.contentContainerEl.append(contenEl);
 
-        setTimeout(() => {
-            this.modalEl.classList.add("open");
-        },0)
-    }
+//         setTimeout(() => {
+//             this.modalEl.classList.add("open");
+//         },0)
+//     }
 
-    renderContent() {
-        return document.createElement("div");
-    }
+//     renderContent() {
+//         return document.createElement("div");
+//     }
 
-    handleClickOpen() {
-        this.onOpenModal();
-        this.render();
-    }
+//     handleClickOpen() {
+//         this.onOpenModal();
+//         this.render();
+//     }
 
-    onCloseModal() {
-        this.modalEl.classList.remove("open");
+//     onCloseModal() {
+//         this.modalEl.classList.remove("open");
 
-        setTimeout(() => {
-            this.modalEl.remove();
-        }, 400);
-    }
+//         setTimeout(() => {
+//             this.modalEl.remove();
+//         }, 400);
+//     }
 
-    addEventListeners() {
-        this.closeEl.addEventListener('click', this.onCloseModal.bind(this));
-        this.overlayEl.addEventListener('click', this.onCloseModal.bind(this))
-    }
-}
+//     addEventListeners() {
+//         this.closeEl.addEventListener('click', this.onCloseModal.bind(this));
+//         this.overlayEl.addEventListener('click', this.onCloseModal.bind(this))
+//     }
+// }
+
 
 // import Modal from "./modal";
 
